7395d5bb701a72af76006f3088f30f42acc1cec8
Sergio-Israel-Hrnz <sergio.israel.hernandez@gmail.com>
2019-10-12 12:01:42 -0600

    estructura del proyecto inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ControlTesisCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
