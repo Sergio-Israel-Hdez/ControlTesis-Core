f2387a29f9625ed13cd1bca5e42fae572023bfa8
Sergio-Israel-Hrnz <sergio.israel.hernandez@gmail.com>
2019-10-12 12:31:31 -0600

    layout principal actualizado

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,7 +22,9 @@ namespace ControlTesisCore.Controllers
         {
             return View();
         }
-
+        public IActionResult Login(){
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
