9b6b8e7f8d34eefa1d0ed9a7c14860fc194ae1b9
Sergio-Israel-Hrnz <sergio.israel.hernandez@gmail.com>
2019-10-21 19:00:32 -0600

    login y modelo perfil modificados

--- Models/BD/EstudiantePerfil.cs
+++ Models/BD/EstudiantePerfil.cs
@@ -5,6 +5,7 @@ namespace ControlTesisCore.Models.BD
 {
     public partial class EstudiantePerfil
     {
+        public int IdUsuario{get;set;}
         public string Nombre{get;set;}
         public string Apellido{get;set;}
         public string Direccion{get;set;}
